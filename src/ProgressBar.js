3e44096e60e3c8ca32915ca9507bdd5839a170e4
Yash Kaskar <yash.kaskar99@gmail.com>
2023-07-05 12:44:33 +0530

    changes

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,11 +1,43 @@
-import React from 'react'
+// import React, { useState, useEffect } from 'react';
 
-const ProgressBar = () => {
-  return (
-    <div>
-      Progress Bar
-    </div>
-  )
-}
+// const ProgressBar = () => {
+//   const [progress, setProgress] = useState(0);
 
-export default ProgressBar
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       // Increment the progress by 1
+//       setProgress((prevProgress) => prevProgress + 1);
+
+//       // Check if progress reaches the width of the page
+//       if (progress >= window.innerWidth) {
+//         clearInterval(timer); // Stop the timer
+//       }
+//     }, 1000);
+
+//     return () => {
+//       clearInterval(timer); // Cleanup function to stop the timer when component unmounts
+//     };
+//   }, [progress]);
+
+//   return (
+//     <>
+//       <div> <h1> Progress Bar </h1> </div>
+// <div
+//   style={{
+//     width: `${progress}px`,
+//     height: '10px',
+//     backgroundColor: 'blue',
+
+//   }}
+// >
+// </div>
+
+//     </>
+    
+//   );
+// };
+
+// export default ProgressBar;
+
+
+

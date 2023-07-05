39c3b3efdc06a9bed83104f5707fb70d637680e7
Yash Kaskar <yash.kaskar99@gmail.com>
2023-07-05 15:25:14 +0530

    progress bar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,43 +1,31 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 
-// const ProgressBar = () => {
-//   const [progress, setProgress] = useState(0);
-
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       // Increment the progress by 1
-//       setProgress((prevProgress) => prevProgress + 1);
-
-//       // Check if progress reaches the width of the page
-//       if (progress >= window.innerWidth) {
-//         clearInterval(timer); // Stop the timer
-//       }
-//     }, 1000);
-
-//     return () => {
-//       clearInterval(timer); // Cleanup function to stop the timer when component unmounts
-//     };
-//   }, [progress]);
-
-//   return (
-//     <>
-//       <div> <h1> Progress Bar </h1> </div>
-// <div
-//   style={{
-//     width: `${progress}px`,
-//     height: '10px',
-//     backgroundColor: 'blue',
-
-//   }}
-// >
-// </div>
-
-//     </>
-    
-//   );
-// };
-
-// export default ProgressBar;
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {  
+    if(progress < window.innerWidth){
+        setTimeout(()=> setProgress(pre => pre += 1), 50)
+    }
+  },[progress])
 
 
 
+  return (
+    <>
+    <div> 
+      <h1> Progress Bar</h1>
+    </div>
+      <div
+      style={{  
+          width: `${progress}px`,
+          height: '100px',
+        backgroundColor:'magenta'
+      }}
+      
+      /> 
+      
+    </>
+  )
+}
+
+export default ProgressBar
